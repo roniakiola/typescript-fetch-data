336ba92dade9a40950e3a3e3f21f959063e4553d
Roni Akiola <roni.akiola@hotmail.com>
2023-05-01 22:51:26 +0300

    fetchAllCompanies now  calls fetchCompany for detailed information

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-// type FetchInputs = {
-//   maxResults: number;
-//   resultFrom: number;
-//   streetAddressPostCode: string;
-// };
+import Company from './types';
 
 const fetchAllCompanies = async (
   maxResults: number,
@@ -13,18 +9,22 @@ const fetchAllCompanies = async (
     `http://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=${maxResults}&resultsFrom=${resultFrom}&streetAddressPostCode=${streetAddressPostCode}`
   );
   const data = await response.json();
-  const result = await data.results;
-  console.log(result);
+  const result: Company[] = await data.results;
+  result.forEach((obj) => {
+    const firstKey = Object.keys(obj)[0] as keyof Company;
+    const businessId = obj[firstKey] as string;
+    fetchCompany(businessId);
+  });
   return result;
 };
 
 const fetchCompany = async (businessId: string) => {
   const response = await fetch(`http://avoindata.prh.fi/bis/v1/${businessId}`);
   const data = await response.json();
-  const result = await data.results;
+  const result = await data.results[0];
   console.log(result);
   return result;
 };
 
 fetchAllCompanies(5, 1, '00200');
-fetchCompany('3346624-5');
+// fetchCompany('3346624-5');
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,9 @@
+interface Company {
+  businessId: string;
+  registrationDate: string;
+  companyForm?: string;
+  detailsUri?: string;
+  name: string;
+}
 
+export default Company;
