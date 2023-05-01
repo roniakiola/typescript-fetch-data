9b9488225042e5afd50a2736b08bfea833e9a0c3
Roni Akiola <roni.akiola@hotmail.com>
2023-05-01 21:22:57 +0300

    fetchCompany

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@
 //   streetAddressPostCode: string;
 // };
 
-const fetchApi = async (
+const fetchAllCompanies = async (
   maxResults: number,
   resultFrom: number,
   streetAddressPostCode: string
@@ -13,9 +13,18 @@ const fetchApi = async (
     `http://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=${maxResults}&resultsFrom=${resultFrom}&streetAddressPostCode=${streetAddressPostCode}`
   );
   const data = await response.json();
-  const result = data.results;
+  const result = await data.results;
   console.log(result);
   return result;
 };
 
-fetchApi(5, 1, '00200');
+const fetchCompany = async (businessId: string) => {
+  const response = await fetch(`http://avoindata.prh.fi/bis/v1/${businessId}`);
+  const data = await response.json();
+  const result = await data.results;
+  console.log(result);
+  return result;
+};
+
+fetchAllCompanies(5, 1, '00200');
+fetchCompany('3346624-5');
